df591ede32592b6f3866a5f566117acea0703b3b
Mekhi Wood-Brock <mekhiwb966@gmail.com>
2022-06-21 09:24:52 -0400

    First day of 100 days to code python

---
+++ final-projects/day-1/band-name-generator.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator")
+City = input("What's the name of the city you grew up in?\n")
+Pet = input("What's the name of your first pet?\n")
+
+Band_name = City + " " + Pet
+print("Your band name could be", Band_name)
